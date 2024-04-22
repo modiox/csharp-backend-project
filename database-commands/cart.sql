926f58a6c259d14fd11d7559e9b42357a9021712
modiox <moodyalyalak@gmail.com>
2024-04-22 15:33:36 +0300

    cart.sql added

---
+++ database-commands/cart.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Cart (
+    cartID SERIAL PRIMARY KEY,
+    FOREIGN KEY(ProductId) REFERENCES products(productID),
+    FOREIGN KEY(customerID) REFERENCES customer(customerID)
+);
+
+--INSERT INTO Cart(productID, customerID) VALUES(1, 1001),
+      -- (2, 1002),
+      -- (3, 1001);
+-- DROP TABLE cart; -- TRUNCATE TABLE cart;
