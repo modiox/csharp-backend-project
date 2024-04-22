a35547e8adae57e0f802d6c80619566bfd4fc626
Malek-Almutairi <malekalmutairi12@gmail.com>
2024-04-22 15:34:32 +0300

    Product table

---
+++ database-commands/product.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Product(
+
+ProductID serial PRIMARY KEY,
+Name  VARCHAR(50) NOT NULL,
+Description TEXT,
+Quantity INT NOT NULL ,
+Price DECIMAL(10, 2) NOT NULL,
+CreatedDate  TIMESTAMP ,
+CategoryID serial FOREIGN KEY, 
+
+);
