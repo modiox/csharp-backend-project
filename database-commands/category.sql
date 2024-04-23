c7a864ecdbc7484efbc6c7c24223be555b9b11ae
modiox <moodyalyalak@gmail.com>
2024-04-23 09:48:25 +0300

    Fixed insertion in category.sql from double quotation to single ones

--- database-commands/category.sql
+++ database-commands/category.sql
@@ -1,19 +1,17 @@
-CREATE TABLE Category(
-    CategoryID SERIAL PRIMARY KEY,
-    Name VARCHAR(50) UNIQUE NOT NULL,
-    Description TEXT,
-);
+CREATE TABLE category(
+    categoryID SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description TEXT);
 
-INSERT INTO Category(Name, Description)
+INSERT INTO category(name, description)
 VALUES
-("Smart Phones","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Headphones","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Tablets","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Power Banks","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Laptops","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Electronics","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Accessories","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("TVs","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Video Games","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit."),
-("Books","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.");
-
+('Smart Phones','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Headphones','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Tablets','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Power Banks','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Laptops','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Electronics','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Accessories','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('TVs','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Video Games','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.'),
+('Books','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor, est in molestie eleifend, justo purus rhoncus ante, tortor velit sit amet elit.');
--- database-commands/product.sql
+++ database-commands/product.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Product(
+CREATE TABLE product(
 
-ProductID serial PRIMARY KEY,
-Name  VARCHAR(50) NOT NULL,
-Description TEXT,
-Quantity INT NOT NULL ,
-Price DECIMAL(10, 2) NOT NULL,
-CreatedDate  TIMESTAMP Default CURRENT_TIMESTAMP ,
-FOREIGN KEY (CategoryID) SERIAL REFERENCES Category(CategoryID) 
+productID serial PRIMARY KEY,
+name  VARCHAR(50) NOT NULL,
+description TEXT,
+quantity INT NOT NULL ,
+price DECIMAL(10, 2) NOT NULL,
+createdDate  TIMESTAMP Default CURRENT_TIMESTAMP ,
+FOREIGN KEY (categoryID) SERIAL REFERENCES category(categoryID) 
 );
