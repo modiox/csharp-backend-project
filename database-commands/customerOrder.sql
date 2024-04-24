b6d005a6f8be3cac8220bbea53a354c5f05d549d
Turki Dhawi <T.Dhawi@outlook.com>
2024-04-24 20:14:44 +0300

    change the name of orderDetails or deleted also adding new column in customerOrder table as well as added some testing queries

--- database-commands/customerOrder.sql
+++ database-commands/customerOrder.sql
@@ -2,19 +2,39 @@ CREATE TYPE orderStatusEnum AS ENUM ('pending', 'processing', 'shipped', 'delive
 
 CREATE TABLE customerOrder(
     orderID SERIAL Primary Key,
-    orderStatus orderStatusEnum,
+    orderStatus orderStatusEnum Default 'pending',
     createdDate TIMESTAMP Default CURRENT_TIMESTAMP,
     customerID INT,
     FOREIGN KEY(customerID) REFERENCES customer(customerID)
 );
 
-INSERT INTO customerOrder (orderStatus, customerID)
-VALUES
-('pending', 1),
-('processing', 2),
-('pending', 3),
-('shipped', 4),
-('pending', 5);
+-- ! The new Table
+-- ! we need to run this query before inserting records  
+ALTER TABLE customerOrder
+ADD payment JSONB;
 
--- SELECT * 
--- FROM customerOrder;
+-- inserting some dummy data
+INSERT INTO customerOrder (orderStatus, customerID, payment)
+VALUES
+('pending', 1, '{"method": "Credit Card", "amount": 50}'),
+('processing', 2, '{"method": "Credit Card", "amount": 100}'),
+('pending', 3, '{"method": "Credit Card", "amount": 55}'),
+('shipped', 4, '{"method": "Credit Card", "amount": 80}'),
+('pending', 5, '{"method": "Credit Card", "amount": 50}');
+
+-- some testing queries
+
+-- start with simple one for testing
+SELECT * 
+FROM customerOrder;
+
+-- it wil return customer id with his order details
+select t1.customerID, t1.orderID, t1.orderStatus, t2.productQuantity, t3.productName, t3.price
+from customerOrder t1 
+inner join orderDetails t2 on t1.orderID = t2.orderID
+inner join product t3 on t2.productID=t3.productID;
+
+-- same as the on above 
+SELECT t1.customerID, t1.orderID, t1.orderStatus, t2.productQuantity, t3.productName, t3.price
+FROM customerOrder t1, orderDetails t2, product t3
+WHERE t1.orderID = t2.orderID AND t2.productID=t3.productID;
--- database-commands/orderDetails.sql
+++ database-commands/orderDetails.sql
@@ -1,3 +1,4 @@
+-- ! I think is better to use an array for productId but I do not know how we can reference it to an integer 
 CREATE TABLE orderDetails (
     orderDetailsID SERIAL PRIMARY KEY,
     orderID INT,
@@ -7,19 +8,41 @@ CREATE TABLE orderDetails (
     FOREIGN KEY (productID) REFERENCES product (productID)
 );
 
+
+-- ! use this query to change the name of orderDetails to orderDetailsID
+ALTER TABLE orderDetails 
+RENAME COLUMN orderDetails TO orderDetailsID;
+
+-- ! I believe we need to delete the orderDetailsID since we are not using it
+-- this is the query for deleting the column
+ALTER TABLE orderDetails
+DROP COLUMN orderDetailsID;
+
+-- inserting some dummy data
 INSERT INTO orderDetails(orderID, productID, productQuantity)
 VALUES
-(1, 4, 5),
-(2, 5, 1),
-(3, 6, 1),
-(4, 4, 1),
-(5, 5, 1);
+(1, 1, 5),
+(2, 2, 1),
+(3, 3, 1),
+(4, 3, 1),
+(5, 3, 1);
 
--- ALTER TABLE orderDetails RENAME COLUMN orderDetails TO orderDetailsID;
 
--- SELECT *
--- FROM orderDetails;
+-- some queries for testing
 
--- SELECT o.*, p.productName, p,price
--- FROM orderDetails o
--- INNER JOIN product p on p.productID = o.productID;
+SELECT *
+FROM orderDetails;
+
+SELECT o.*, p.productName, p,price
+FROM orderDetails o
+INNER JOIN product p on p.productID = o.productID;
+
+-- here if we using an array for productID
+
+SELECT p.*
+FROM product p 
+JOIN (
+    SELECT UNNEST(productIDs) as productID
+    FROM orderDetails
+    WHERE orderDetailsID = 1 
+) o ON p.productID = o.productID;
