b2e368a60a5d722969b5d01442a8be04bbb3fd0c
modiox <moodyalyalak@gmail.com>
2024-04-28 14:02:19 +0300

    Added clarifications and comments for the changes

--- database-commands/cart.sql
+++ database-commands/cart.sql
@@ -1,9 +1,23 @@
+
+-- Old cart table definition
 CREATE TABLE cart (
     productID INT,
     customerID INT, 
     FOREIGN KEY (productID) REFERENCES product (productID),
     FOREIGN KEY (customerID) REFERENCES customer (customerID)
+); 
+
+-- New Cart table definition 
+
+CREATE TABLE cart (
+    productID INT,
+    customerID INT, 
+    FOREIGN KEY (productID) REFERENCES product (productID),
+    FOREIGN KEY (userID) REFERENCES users (userID)
 );
+
+
+-- Alter queires entered in pgAdmin instead of dropping the entire table
 
 ALTER TABLE cart
 DROP CONSTRAINT cart_customerid_fkey; 
