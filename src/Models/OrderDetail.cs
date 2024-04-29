7c73f828fa82bae54f89984db38dfbd48fedd4ed
ReemOthm <ReemOth@outlook.com>
2024-04-29 17:09:58 +0300

    added a default values to User, renamed OrderDetailsID

--- src/Models/OrderDetail.cs
+++ src/Models/OrderDetail.cs
@@ -1,6 +1,6 @@
 public class OrderDetail
 {
-  public required Guid OrderDetailsId { get; set; }
+  public required Guid CustomerId { get; set; }
   public required Guid ProductId { get; set; }
   public required int ProductQuantity { get; set; }
   public required string Payment { get; set; }
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -2,35 +2,34 @@ using System.ComponentModel.DataAnnotations;
 
 public class User
 {
-    public int UserID { get; set; }
+    public Guid UserID { get; set; } = Guid.NewGuid();
 
-    [Required(ErrorMessage = "Username is required")]
-    [StringLength(50)]
-    public string Username { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Username is required")][StringLength(50)] 
+    public required string Username { get; set; }
 
     [Required(ErrorMessage = "Email is required")]
     [StringLength(100)]
-    public string Email { get; set; } = string.Empty;
+    public required string Email { get; set; }
 
     [Required(ErrorMessage = "Password is required")]
     [StringLength(32)]
-    public string Password { get; set; } = string.Empty;
+    public required string Password { get; set; }
 
     [Required(ErrorMessage = "First name is required")]
     [StringLength(20)]
-    public string FirstName { get; set; } = string.Empty;
+    public required string FirstName { get; set; }
 
     [Required(ErrorMessage = "Last name is required")]
     [StringLength(20)]
-    public string LastName { get; set; } = string.Empty;
+    public required string LastName { get; set; } 
 
     [StringLength(20)]
     public string PhoneNumber { get; set; } = string.Empty;
 
     [StringLength(255)]
     public string Address { get; set; } = string.Empty;
-    public bool IsAdmin { get; set; }
-    public bool IsBanned { get; set; }
+    public bool IsAdmin { get; set; } = false; 
+    public bool IsBanned { get; set; } = false; 
     public DateTime BirthDate { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
