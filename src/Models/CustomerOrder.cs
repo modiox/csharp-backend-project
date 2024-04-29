4166f3ff599efecc993553469dc2d6604e08ed6b
ReemOthm <ReemOth@outlook.com>
2024-04-29 21:12:36 +0300

    Modified CustomerOrder Model

--- src/Models/CustomerOrder.cs
+++ src/Models/CustomerOrder.cs
@@ -1,8 +1,16 @@
-public enum Status {pending, processing, shipped, delivered};
+public enum Status { pending, processing, shipped, delivered };
+
 public class CustomerOrder
 {
-  public required Guid CustomerId { get; set; }
-  public required Guid OrderId { get; set; }
-  public enum Status { get, set }
+
+  public Guid OrderId { get; set; } = Guid.NewGuid();
+
+  public required Guid UserId { get; set; }
+
+  public Status OrderStatus { get; set; } = Status.pending;
+
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+
   public required string Payment { get; set; }
+
 }
