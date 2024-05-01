91b498e831592eefc9b035301810ea8cfbd06092
Turki Dhawi <T.Dhawi@outlook.com>
2024-05-01 22:55:06 +0300

    Added CustomerOrder Model

--- src/Models/CustomerOrderModel.cs
+++ src/Models/CustomerOrderModel.cs
@@ -1,8 +1,25 @@
-public enum Status {pending, processing, shipped, delivered};
-public class CustomerOrder
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+public enum OrderStatus { Creating = 0, Pending = 1, Processing = 2, Shipped = 3, Delivered = 4 };
+public class CustomerOrderModel
 {
-  public required Guid CustomerId { get; set; }
+  [Required(ErrorMessage = "Order Id is required")]
   public required Guid OrderId { get; set; }
-  public enum Status { get, set }
+
+  [Required(ErrorMessage = "Order Status is required")]
+  public required OrderStatus Status { get; set; }
+
+  [Required(ErrorMessage = "Payment method is required")]
   public required string Payment { get; set; }
+
+  [Required(ErrorMessage = "Product Id is required")]
+  public required Guid ProductId { get; set; }
+
+  // public virtual List<ProductModel>? Product { get; set; }
+
+  [Required(ErrorMessage = "User Id is required")]
+  public Guid UserId { get; set; }
+
+  // public virtual UserModel? User { get; set; }
 }
--- src/Models/OrderDetailModel.cs
+++
@@ -1,7 +0,0 @@
-public class OrderDetail
-{
-  public required Guid CustomerId { get; set; }
-  public required Guid ProductId { get; set; }
-  public required int ProductQuantity { get; set; }
-  public required string Payment { get; set; }
-}
