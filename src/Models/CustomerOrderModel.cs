37581ad10a4d96cba2bc49f45f8cb2418b731fa1
ReemOthm <ReemOth@outlook.com>
2024-05-01 23:26:07 +0300

    added Customer Order Model

--- src/Models/CustomerOrderModel.cs
+++ src/Models/CustomerOrderModel.cs
@@ -1,16 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models{
+    
 public enum Status { pending, processing, shipped, delivered };
 
-public class CustomerOrder
+public enum PaymentMethod { CreditCard, ApplePay, Visa, Cash, PayPal };
+
+public record class OrderPayment(
+    [Required(ErrorMessage = "Payment Method is required!")]
+    PaymentMethod Method,
+
+    [Required(ErrorMessage = "Amount is Required!")]
+    [Range(0, double.MaxValue, ErrorMessage ="The amount should be more than 0!")]
+    double Amount
+);
+
+public class CustomerOrderModel
 {
 
-  public Guid OrderId { get; set; } = Guid.NewGuid();
+    public Guid OrderId { get; set; } = Guid.NewGuid();
 
-  public required Guid UserId { get; set; }
+    [Required(ErrorMessage = "User Id is required!")]
+    public required Guid UserId { get; set; }
 
-  public Status OrderStatus { get; set; } = Status.pending;
+    // public virtual User? User { get; set; }
 
-  public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public Status OrderStatus { get; set; } = Status.pending;
 
-  public required string Payment { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 
+    public required OrderPayment Payment { get; set; }
+
+}
 }
