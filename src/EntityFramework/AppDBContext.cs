8278afd009ad1da7486cb3129f44345f01f3bec3
Modi Alyalak <36824388+modiox@users.noreply.github.com>
2024-05-01 23:56:25 +0300

    Update AppDBContext.cs

--- src/EntityFramework/AppDBContext.cs
+++ src/EntityFramework/AppDBContext.cs
@@ -6,11 +6,11 @@ public class AppDBContext: DbContext  {
     public AppDBContext(DbContextOptions options) : base(options){}
     
     // DbSet properties for our entities
-    // public DbSet<User> Users { get; set; }
+     public DbSet<User> Users { get; set; }
     // public DbSet<Product>Products {get; set; }
     public DbSet<Category> Categories {get; set; }
     // public DbSet<OrderDetail> Orders {get; set; }
-    // public DbSet<CustomerOrder> CustomerOrders {get; set; }
+     public DbSet<CustomerOrder> CustomerOrders {get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
