11a05e595c3173a32a8ea246ff59ccb38f01815d
Turki Dhawi <T.Dhawi@outlook.com>
2024-05-11 05:21:38 +0300

    updated cartModel

--- src/Models/CartModel.cs
+++ src/Models/CartModel.cs
@@ -7,6 +7,6 @@ public class CartModel
     public Guid ProductID { get; set; }
     [Required(ErrorMessage = "User Id is required")]
     public Guid UserID { get; set; }
-    public ProductModel Product { get; set; }
-    public UserModel User { get; set; }
+    public ProductModel? Product { get; set; }
+    public UserModel? User { get; set; }
 }
